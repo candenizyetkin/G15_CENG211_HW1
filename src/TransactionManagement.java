be23a078d8d99e59e64eec7c9ab6d34c4ae4c4da
Can Deniz Yetkin <candeniz1@outlook.com>
2023-10-26 20:22:54 +0300

    ömnkj

--- src/TransactionManagement.java
+++ src/TransactionManagement.java
@@ -1,4 +1,43 @@
-
+import java.util.Random;
 public class TransactionManagement {
+	
+	private Product[] productArray =FileIO.readProductData(null)
+	private int [] idNumber;
+		TransactionManagement(int[] idNumber,Product[] productArray ){
+			this.productArray=productArray;
+		}
+		
+		
+		public void randomProductAssigner() {
+			 Random random = new Random();
+			 int numberOfElementsToSelect = 100;
+			 
+			 
+			 for (int i = 0; i < numberOfElementsToSelect; i++) {
+				 Product[] selectedProduct = new Product[numberOfElementsToSelect];
+				 int j=0;
+		            int randomIndex = random.nextInt(idNumber.length);
+		            int selectedProductId = idNumber[randomIndex];
+		            while(selectedProduct[3]==null) {
+		            if(productArray[i].id==selectedProductId) {
+		            selectedProduct[j]=productArray[i];
+		            }}
+		            Transaction transaction = new Transaction(3,selectedProductId,4,5);
+		            selectedTransactions[i] = transaction;
+		        }
+			 
+		}
+		
+		public void randomProductAssigner() {
+			
+		}
+		
+		public Product productIdMatch(){
+			
+			for (int i = 0; i < )
+			
+		}
+		
+		
 		
 }
