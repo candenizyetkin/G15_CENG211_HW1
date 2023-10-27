bc01c0819b78364bcc315ae0adc80c9020778198
berkyusuf <123365168+berkyusuf@users.noreply.github.com>
2023-10-27 23:24:03 +0300

    constracter

--- src/Query.java
+++ src/Query.java
@@ -1,5 +1,11 @@
 
 public class Query {
+	ShopAssistant[] shopAssistant;
+	Transaction[][]	transaction;
+	Query(ShopAssistant[] shopAssistant,Transaction[][] transaction){
+		this.shopAssistant=shopAssistant;
+		this.transaction=transaction;
+	}
 	public double highestTotalPrice(){}
 	public String mostExpensiveProductInTheLowestPriceTransaction(){}
 	public double lowestTransactionFee(){}
