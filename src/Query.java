1a2e0eea4f0b456ce009b88d167681e55be1e6f8
berkyusuf <123365168+berkyusuf@users.noreply.github.com>
2023-10-27 23:17:59 +0300

    query

---
+++ src/Query.java
@@ -0,0 +1,9 @@
+
+public class Query {
+	public double highestTotalPrice(){}
+	public String mostExpensiveProductInTheLowestPriceTransaction(){}
+	public double lowestTransactionFee(){}
+	public String highestSalaryShopAssistant(){}
+	public double totalRevenue(){}
+	public double totalProfit(){}
+}
